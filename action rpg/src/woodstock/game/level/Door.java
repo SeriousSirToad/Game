a03e90a11d9132ea1f9f76b727c5a125f11bc5ec
SeriousSirToad <34554852+SeriousSirToad@users.noreply.github.com>
2018-12-11 10:59:18 -0500

    Update Door.java

--- action rpg/src/woodstock/game/level/Door.java
+++ action rpg/src/woodstock/game/level/Door.java
@@ -6,12 +6,22 @@ public class Door {
 
 	Rectangle rect;
 	Level level;
+	Point tpLocation;
 	
-	public Door(Rectangle rect, Level level) {
+	public Door(Rectangle rect, Level level, Point tpLocation) {
 		
 		this.rect = rect;
 		this.level = level;
+		this.tpLocation = tpLocation;
 		
+	}
+	
+	public void tick(){
+		if(rect.intersects(GameState.player.collider){
+			GameState.player.setLevel(level);
+			GameState.player.x = tpLocation.getX();
+			GameState.player.y = tpLocation.getY();
+		}
 	}
 
 }
